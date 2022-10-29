3e312eed47e4bc96df84b7baa6054898eec6c55e
Sachin (Ranganath) K <ranganathkulkarni20@gmail.com>
2022-10-29 12:18:16 +0530

    Added foreach()

---
+++ forEach.js
@@ -0,0 +1,13 @@
+let persons=['Ranganath', 'Sachin', 'Anand'];
+
+persons.forEach(function(person){
+                console.log(person, person[0]);
+})
+
+/*Output
+"Ranganath" "R"
+"Sachin" "S"
+"Anand" "A"
+*/
+
+
